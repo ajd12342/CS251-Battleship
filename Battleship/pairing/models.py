2433f3512f15c6d068d1670d8bb48be7f2fa5b9a
DIWAN ANUJ JITENDRA <anujdiwan@cse.iitb.ac.in>
2018-10-17 02:11:06 +0530

    Extended the User model with an isAvailable boolean and set receivers to flip it. Improved unauthenticated scenario using login_required decorator. Implemented the list that displays all available users. Login works, logout fails to set isAvailable to False.

--- Battleship/pairing/models.py
+++ Battleship/pairing/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.signals import user_logged_in,user_logged_out
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    isAvailable= models.BooleanField(default= False)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+@receiver(user_logged_in, sender=User)
+def on_login(sender, request, user, **kwargs):
+	user.profile.isAvailable=True
+
+@receiver(user_logged_out, sender=User)
+def on_logout(sender, request, user, **kwargs):
+	user.profile.isAvailable=False
--- Battleship/pairing/views.py
+++ Battleship/pairing/views.py
@@ -5,6 +5,8 @@ from django.template import loader
 from django.shortcuts import render,get_object_or_404
 from django.views import generic
 from django.contrib.auth.models import Permission, User
+from .models import Profile
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
 	if(not request.user.is_authenticated):
@@ -12,8 +14,10 @@ def index(request):
 	else:
 		return HttpResponseRedirect('/pairing/profile/')
 
+@login_required
 def profile(request):
 	return render(request,'pairing/profile.html',{})
-
+	
+@login_required
 def list(request):
-	return HttpResponse("This should show a list of logged in players")
+	return render(request,'pairing/list.html',{'profiles':Profile.objects.filter(isAvailable=True)})
