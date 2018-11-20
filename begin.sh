93976498ad953faa1af5e8afb8c704a80c1cf141
Soumya Chatterjee <33203398+soumyac1999@users.noreply.github.com>
2018-11-21 01:20:01 +0530

    Create begin.sh

---
+++ begin.sh
@@ -0,0 +1,12 @@
+sudo apt install python-dev
+sudo apt install python3-pip
+sudo apt install redis
+pip3 install -U django
+pip3 install -U django-crispy-forms
+pip3 install -U channels
+pip3 install -U channels-redis
+pip3 install -U django-picklefield
+
+cd Battleship
+redis-server --port 10000 &
+python3 manage.py runserver
