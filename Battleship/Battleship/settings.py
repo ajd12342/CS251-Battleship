7563ef8e4da819d8fbb109b66f9d8f9d2ae5b7fe
DIWAN ANUJ JITENDRA <anujdiwan@cse.iitb.ac.in>
2018-11-23 03:59:23 +0530

    Enabled playing on different devices.

--- Battleship/Battleship/settings.py
+++ Battleship/Battleship/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8(l^=ia%c8474)84m&ez0za!x-ku1cw3s&#!m8d13n4_zb28b@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','192.168.43.174']
 
 
 # Application definition
