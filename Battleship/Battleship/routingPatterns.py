707e858638330a3ac22cfe0bf9a137a03a7959ef
rahul <rahul265653@gmail.com>
2018-11-23 03:54:20 +0530

    Added chat interface in the play page

--- Battleship/Battleship/routingPatterns.py
+++ Battleship/Battleship/routingPatterns.py
@@ -3,9 +3,10 @@ from django.urls import path,include
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from pairing.consumers import ChatConsumer
-from play.consumers import SubmitConsumer,GameConsumer
+from play.consumers import SubmitConsumer,GameConsumer,GameChatConsumer
 websocket_urlpatterns = [
     path('users/',ChatConsumer),
     path('placing/',SubmitConsumer),
     path('game/',GameConsumer),
+    path('chat/',GameChatConsumer),
 ]
