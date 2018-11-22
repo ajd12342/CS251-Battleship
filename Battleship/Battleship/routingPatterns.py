cbd6681f1c8c6096e34a06f5ee8282ca28c93e7b
DIWAN ANUJ JITENDRA <anujdiwan@cse.iitb.ac.in>
2018-11-22 19:01:21 +0530

    Finished fixing bugs of placing ships. Made game page and constructed the game from initial state.

--- Battleship/Battleship/routingPatterns.py
+++ Battleship/Battleship/routingPatterns.py
@@ -3,8 +3,9 @@ from django.urls import path,include
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from pairing.consumers import ChatConsumer
-from play.consumers import SubmitConsumer
+from play.consumers import SubmitConsumer,GameConsumer
 websocket_urlpatterns = [
     path('users/',ChatConsumer),
     path('placing/',SubmitConsumer),
+    path('game/',GameConsumer),
 ]
