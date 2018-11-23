432f9a3ef5d475b5d18ad5569ae0c0917310a815
Soumya Chatterjee <33203398+soumyac1999@users.noreply.github.com>
2018-11-24 02:15:36 +0530

    Added net tools installation

--- begin.sh
+++ begin.sh
@@ -1,6 +1,7 @@
 sudo apt install python-dev
 sudo apt install python3-pip
 sudo apt install redis-server
+sudo apt install net-tools
 pip3 install -U django
 pip3 install -U django-crispy-forms
 pip3 install -U channels
